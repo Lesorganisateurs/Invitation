d8084bc268c2f90842d74d348b72174df600299c
Lesorganisateurs <148695023+Lesorganisateurs@users.noreply.github.com>
2023-11-05 13:26:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ function checkPassword() {
     var password = document.getElementById("password").value;
     
     if (password === "Caca") {
+        // Masquer le cadre mot de passe
         document.getElementById("password").style.display = "none";
+        // Masquer l'image en changeant la valeur de display à "none"
+            document.getElementById("image").style.display = "none";
+        // Afficher le message d'invitation
         document.getElementById("invitation").style.display = "block";
     } else {
         alert("Mot de passe incorrect. Veuillez réessayer.");
