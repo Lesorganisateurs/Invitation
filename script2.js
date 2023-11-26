7739801bc21d53ae452e9e64ca19ca44ec611eea
Lesorganisateurs <148695023+Lesorganisateurs@users.noreply.github.com>
2023-11-26 14:23:30 +0100

    Update script2.js

--- script2.js
+++ script2.js
@@ -36,4 +36,33 @@ function checkPassword(expectedPassword, nextPage) {
     } else {
         document.getElementById("error-message").innerHTML = "Mot de passe incorrect. Veuillez réessayer.";
     }
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Le reste du code pour le décompteur reste inchangé
+
+    // Ajoutez la fonction pour basculer entre les types d'entrée de mot de passe
+    var showPasswordCheckbox = document.getElementById("show-password");
+    var passwordInput = document.getElementById("password");
+
+    if (showPasswordCheckbox) {
+        showPasswordCheckbox.addEventListener("change", function () {
+            if (showPasswordCheckbox.checked) {
+                passwordInput.type = "text";
+            } else {
+                passwordInput.type = "password";
+            }
+        });
+    }
+});
+
+function checkPassword(expectedPassword, nextPage) {
+    var password = document.getElementById("password").value;
+
+    if (password === expectedPassword) {
+        window.location.href = nextPage;
+    } else {
+        document.getElementById("error-message").innerHTML = "Mot de passe incorrect. Veuillez réessayer.";
+    }
+}
+
 }
